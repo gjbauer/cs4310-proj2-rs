b62d5888151193e2a5fb32ec1124cf89c955325f
Gabriel Bauer <gabeb1277@gmail.com>
2025-05-08 00:03:49 -0400

    inode_read()

--- src/inode.rs
+++ src/inode.rs
@@ -1,4 +1,6 @@
 
+const ins: usize = 5 * 4096;	// get_root_start();
+
 pub struct Inode {
 	pub refs: u32,
 	pub mode: u32,
@@ -10,7 +12,6 @@ pub struct Inode {
 
 // TODO: Implement inode_deserialize...
 pub fn inode_deserialize(mmap: &memmap2::MmapMut, num: usize) -> Inode {
-	let ins: usize = 5 * 4096;	// get_root_start();
 	let offset: usize = num * std::mem::size_of::<Inode>();
 	
 	let mut data: [u8; 4] = [0; 4];
@@ -63,8 +64,15 @@ pub fn inode_deserialize(mmap: &memmap2::MmapMut, num: usize) -> Inode {
 	return Inode { refs: refs, mode: mode, size: sizes, ptrs: ptrs, iptr: iptr, inum: inum };
 }
 
-pub fn inode_read() -> [u8] {
-
+pub fn inode_read(d: Inode, mmap: &memmap2::MmapMut) -> Vec<u8> {
+	let mut c: Vec<u8> = vec![];
+	for i in 0..=d.size[0]-1 {
+		c.push(mmap[ins+(d.ptrs[0] as usize)..ins+(d.ptrs[0] as usize)+1][0]);
+	}
+	for i in 0..=d.size[1]-1 {
+		c.push(mmap[ins+(d.ptrs[1] as usize)..ins+(d.ptrs[1] as usize)+1][0]);
+	}
+	return c;
 }
 
 
