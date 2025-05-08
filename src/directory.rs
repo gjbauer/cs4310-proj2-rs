f6f4a15cb93936e6be8b89088206754d3c275c1c
Gabriel Bauer <gabeb1277@gmail.com>
2025-05-08 00:19:05 -0400

    const && corectedd

--- src/directory.rs
+++ src/directory.rs
@@ -1,4 +1,4 @@
-
+const data_start:usize = 5 * 4096;	// get_root_start();
 pub const DIR_NAME: usize = 48;
 
 pub struct Dirent {
@@ -9,19 +9,17 @@ pub struct Dirent {
 
 // TODO: Implement dirent_deserialize...
 pub fn dirent_deserialize(mmap: &memmap2::MmapMut, offset: usize) -> Dirent {
-	let start:usize = 5 * 4096;	// get_root_start();
-	
 	let mut name: [char; DIR_NAME] = ['\0'; 48];
 	
 	for i in 0..=DIR_NAME-1 {
-		let data = mmap[start+offset+i..start+offset+i+1][0];
+		let data = mmap[data_start+offset+i..data_start+offset+i+1][0];
 		name[i] = data as char;
 	}
 	
-	let data = mmap[start+offset+49..start+offset+50][0];
+	let data = mmap[data_start+offset+49..data_start+offset+50][0];
 	let inum = data as u16;
 	
-	let data = mmap[start+offset+51..start+offset+52][0];
+	let data = mmap[data_start+offset+51..data_start+offset+52][0];
 	let active = data != 0;
 	
 	return Dirent { name: name, inum: inum, active: active } ;
