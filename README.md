66a6d4a9a013ddbf2aa38b14be4a03b7484f9733
Gabriel Bauer <gabeb1277@gmail.com>
2025-05-30 21:11:40 -0400

    using fuser...think that is the best direction...

--- README.md
+++ README.md
@@ -5,10 +5,10 @@
 
 ## ~~Step 1: Learn the process behind bindgen and writing build scripts~~
 
-## ~~Step 2: Write the~~ interface between our now greatly minimized C code and Rust
+## ~~Step 2: Write the interface between our now greatly minimized C code and Rust~~
 
-## Step 3: Finish the last few details in terms of implementaion without fuser
+## Step 3: Finish the last few details in terms of implementaion without fuse
 
-## Step 4: Understand more about the fuser/Rust interface and implement the functions
+## Step 4: Understand more about the fuse/Rust interface and implement the functions
 
 ## Step 5: Test.
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,124 @@
-extern crate fuse;
-extern crate libc;
-
-use std::env;
-use std::path::Path;
-use std::slice;
-use libc::{ENOENT, ENOSYS};
+use fuser::{
+    FileAttr, FileType, Filesystem, MountOption, ReplyAttr, ReplyData, ReplyDirectory, ReplyEntry,
+    Request,
+};
+use libc::ENOENT;
 use std::ffi::OsStr;
-use fuse::{FileAttr, FileType, Filesystem, Request, ReplyAttr, ReplyData, ReplyEntry, ReplyDirectory};
-mod inode;
-mod directory;
-mod disk;
-mod hash;
+use std::time::{Duration, UNIX_EPOCH};
 
-struct Nufs;
+const TTL: Duration = Duration::from_secs(1); // 1 second
 
-impl Filesystem for Nufs {
-	fn getattr(&mut self, _req: &Request, ino: u64, reply: ReplyAttr) {
-		println!("getattr(ino={})", ino);
-	}
-	fn readdir(&mut self, _req: &Request, ino: u64, fh: u64, offset: i64, mut reply: ReplyDirectory) {
-		println!("readdir(ino={}, fh={}, offset={})", ino, fh, offset);
-		reply.error(ENOSYS);
-	}
-	fn mknod(&mut self, _req: &Request<'_>, _parent: u64, _name: &OsStr, _mode: u32, _rdev: u32, reply: ReplyEntry,) {
-		println!("mknod({:?})", _name);
-	}
+const HELLO_DIR_ATTR: FileAttr = FileAttr {
+    ino: 1,
+    size: 0,
+    blocks: 0,
+    atime: UNIX_EPOCH, // 1970-01-01 00:00:00
+    mtime: UNIX_EPOCH,
+    ctime: UNIX_EPOCH,
+    crtime: UNIX_EPOCH,
+    kind: FileType::Directory,
+    perm: 0o755,
+    nlink: 2,
+    uid: 501,
+    gid: 20,
+    rdev: 0,
+    flags: 0,
+    blksize: 512,
+};
+
+const HELLO_TXT_CONTENT: &str = "Hello World!\n";
+
+const HELLO_TXT_ATTR: FileAttr = FileAttr {
+    ino: 2,
+    size: 13,
+    blocks: 1,
+    atime: UNIX_EPOCH, // 1970-01-01 00:00:00
+    mtime: UNIX_EPOCH,
+    ctime: UNIX_EPOCH,
+    crtime: UNIX_EPOCH,
+    kind: FileType::RegularFile,
+    perm: 0o644,
+    nlink: 1,
+    uid: 501,
+    gid: 20,
+    rdev: 0,
+    flags: 0,
+    blksize: 512,
+};
+
+struct HelloFS;
+
+impl Filesystem for HelloFS {
+    fn lookup(&mut self, _req: &Request, parent: u64, name: &OsStr, reply: ReplyEntry) {
+        if parent == 1 && name.to_str() == Some("hello.txt") {
+            reply.entry(&TTL, &HELLO_TXT_ATTR, 0);
+        } else {
+            reply.error(ENOENT);
+        }
+    }
+
+    fn getattr(&mut self, _req: &Request, ino: u64, _fh: Option<u64>, reply: ReplyAttr) {
+        match ino {
+            1 => reply.attr(&TTL, &HELLO_DIR_ATTR),
+            2 => reply.attr(&TTL, &HELLO_TXT_ATTR),
+            _ => reply.error(ENOENT),
+        }
+    }
+
+    fn read(
+        &mut self,
+        _req: &Request,
+        ino: u64,
+        _fh: u64,
+        offset: i64,
+        _size: u32,
+        _flags: i32,
+        _lock: Option<u64>,
+        reply: ReplyData,
+    ) {
+        if ino == 2 {
+            reply.data(&HELLO_TXT_CONTENT.as_bytes()[offset as usize..]);
+        } else {
+            reply.error(ENOENT);
+        }
+    }
+
+    fn readdir(
+        &mut self,
+        _req: &Request,
+        ino: u64,
+        _fh: u64,
+        offset: i64,
+        mut reply: ReplyDirectory,
+    ) {
+        if ino != 1 {
+            reply.error(ENOENT);
+            return;
+        }
+
+        let entries = vec![
+            (1, FileType::Directory, "."),
+            (1, FileType::Directory, ".."),
+            (2, FileType::RegularFile, "hello.txt"),
+        ];
+
+        for (i, entry) in entries.into_iter().enumerate().skip(offset as usize) {
+            // i + 1 means the index of the next entry
+            if reply.add(entry.0, (i + 1) as i64, entry.1, entry.2) {
+                break;
+            }
+        }
+        reply.ok();
+    }
 }
 
 fn main() {
-	let mountpoint = match env::args().nth(1) {
-		Some(path) => path,
-		None => {
-			println!("Usage: {} <MOUNTPOINT>", env::args().nth(0).unwrap());
-			return;
-		}
-	};
-	
+    let mountpoint = argv[1];
+    
+    let mut options = vec![MountOption::RO, MountOption::FSName("PFS".to_string())];
+    
+    fuser::mount2(HelloFS, mountpoint, &options).unwrap();
+}
 	/*unsafe {
 		disk::storage_init();
 		let ptr = disk::read_d(52, 52+(5*4096));
@@ -48,7 +131,4 @@ fn main() {
 		let c: &mut [i8] = &mut d;
 		disk::write_d(c.as_mut_ptr(), 52, 52+(5*4096));
 	}*/
-	
-	
-	fuse::mount(Nufs, &mountpoint, &[]);
-}
+
